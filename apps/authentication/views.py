b42a786b2ab15321149fbd1ead06cfefe633a471
Maitland McConnell <maitlandm@gmail.com>
2016-11-15 19:00:14 -0800

    fixed gitignore

--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -31,9 +31,9 @@ class AccountViewSet(viewsets.ModelViewSet):
         if serializer.is_valid():
             Account.objects.create_user(**serializer.validated_data)
 
-            return Response(serializer.validated_data, status=status.HTTP_201_CREATED)
+            return response(serializer.validated_data, status=status.HTTP_201_CREATED)
 
-        return Response({
+        return response({
             'status': 'Bad request',
             'message': 'Account could not be created with received data'
             }, status=status.HTTP_400_BAD_REQUEST)
